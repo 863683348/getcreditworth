cfe7ecc1ab74c05a528f4d28b64397d1a8b0a05b
863683348 <863683348@qq.com>
2026-08-14 23:10:30 +0800

    seo(blog): article-level og image/url and twitter card on post pages

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { getPost, getPostSlugs } from "@/lib/api/controllers/blog.controller";
 import { ArticleJsonLd, BreadcrumbListJsonLd, FaqPageJsonLd } from "@/components/seo/JsonLd";
 import { buildCanonicalUrl } from "@/lib/utils/affiliate";
+import { SITE_CONFIG } from "@/lib/config";
 import { BlogDetailContent } from "@/components/BlogDetailContent";
 import { Breadcrumb } from "@/components/Breadcrumb";
 import { RelatedArticles } from "@/components/RelatedArticles";
@@ -31,7 +32,24 @@ export function generateMetadata({ params }: PageProps): Metadata {
       title: post.title,
       description: post.description,
       type: "article",
+      url: buildCanonicalUrl("/blog/" + post.slug),
       publishedTime: post.date,
+      siteName: SITE_CONFIG.name,
+      images: [
+        {
+          url: `${SITE_CONFIG.url}/og-image.svg`,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: post.title,
+      description: post.description,
+      creator: SITE_CONFIG.twitter,
+      images: [`${SITE_CONFIG.url}/og-image.svg`],
     },
   };
 }
